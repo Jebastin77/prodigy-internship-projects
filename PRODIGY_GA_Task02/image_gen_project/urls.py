b1c264443b4355319453663ff2d7aef87298c078
Jebastin77 <jebastin302007@example.com>
2026-06-22 21:00:03 +0530

    My local changes

--- PRODIGY_GA_Task02/image_gen_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path("", include("image_gen_project.generator.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
